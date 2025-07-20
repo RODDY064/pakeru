2ce96093700635a794814499e7865a7c1a2dc582
Roddy <124875320+RODDY064@users.noreply.github.com>
2025-07-20 00:33:16 +0000

    action tokens added

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from "@playwright/test";
 
 test.use({ trace: "on" });
 
+
 test("has title", async ({ page, baseURL }) => {
   await page.goto(baseURL || "http://localhost:3000");
   await expect(page).toHaveTitle(/E-come/i);
