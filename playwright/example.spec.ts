aa64694b607cfb425825b9ee108ce93d66f54af0
Roddy <124875320+RODDY064@users.noreply.github.com>
2025-08-30 20:12:22 +0000

    test pass

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -5,6 +5,10 @@ test.use({ trace: "on" });
 
 
 test("has title", async ({ page, baseURL }) => {
-  await page.goto(baseURL || "http://localhost:3000");
+
+  // 
+  const url = process.env.NEXT_PUBLIC_URL
+
+  await page.goto(baseURL || `${url}`);
   await expect(page).toHaveTitle(/E-come/i);
 });
