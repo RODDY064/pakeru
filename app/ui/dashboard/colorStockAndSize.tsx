0bc97be841ef5ecdd9f3ee5961527b31ada9d378
Roddy <124875320+RODDY064@users.noreply.github.com>
2025-09-08 22:24:34 +0000

    import issue fixed

--- app/ui/dashboard/colorStockAndSize.tsx
+++ app/ui/dashboard/colorStockAndSize.tsx
@@ -1,4 +1,4 @@
-import { ProductColor } from '@/app/(dashboard)/store-products/product-actions/page';
+import { ProductColor } from '@/app/(dashboard)/store-products/product-actions/action';
 import Image from 'next/image';
 import React, { useState } from 'react'
 
